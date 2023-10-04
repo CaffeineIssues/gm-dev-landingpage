e0e226517928fd7cfd6a255fd7b81424f2d9dfe1
CaffeineIssues <gmdoas.dev@gmail.com>
2023-10-04 02:23:25 -0300

    logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import React from 'react';
 import Image from 'next/image';
 export default function Home() {
-  return <div className="md:container md:mx-auto"></div>;
+  return (
+    <div className="md:container md:mx-auto">
+      <Image
+        src="/logo/white.png"
+        alt="logo"
+        width="640"
+        height="640"
+        className="absolute bottom-10"
+      />
+    </div>
+  );
 }

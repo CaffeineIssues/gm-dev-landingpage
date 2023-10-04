e431079195e41e685730926da61b133fdead1dba
CaffeineIssues <gmdoas.dev@gmail.com>
2023-10-04 00:33:10 -0300

    sidebar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import './globals.css';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
-import Header from './Theme/header/main';
+import Header from './Theme/Header/Main';
 
 const inter = Inter({ subsets: ['latin'] });
 

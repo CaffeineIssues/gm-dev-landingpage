3f6d143e72d45f9b4c44268b1feac2ae6b144813
CaffeineIssues <gmdoas.dev@gmail.com>
2023-10-04 01:39:07 -0300

    staged

--- src/app/Theme/header/Main.tsx
+++ src/app/Theme/header/Main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { TbHome } from 'react-icons/tb';
-import NavButton from './Buttons/Nav';
+import NavButton from './Buttons/NavButton';
 export default function Header() {
   return (
     <>
